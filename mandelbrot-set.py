38ef4e65da6a77a95b8e457d31270eef208a0831
AgentElement <38045210+AgentElement@users.noreply.github.com>
2019-04-21 13:01:31 +0530

    Fixed bug where you couldn't input negative reals

--- mandelbrot-set.py
+++ mandelbrot-set.py
@@ -1,12 +1,15 @@
 from generator import JuliaGenerator, MandelbrotGenerator
 from imager import Imager
 import time
+import sys
 import argparse
 import re
 
 
 def convert_to_complex(z):
+    z = re.sub(r'@', '-', z)
     z = re.sub(r'[iIJ]', 'j', z)
+    z = re.sub(r'[\'\" \t]', '', z)
     return complex(z)
 
 
@@ -15,7 +18,8 @@ def parse():
 
     parser.add_argument('-f', '--focus',
                         help='Focus of the generator.',
-                        required=True)
+                        required=True,
+                        type=convert_to_complex)
 
     generator = parser.add_mutually_exclusive_group(required=True)
 
@@ -25,12 +29,15 @@ def parse():
 
     generator.add_argument('-j', '--julia',
                            help='Generates a julia set for C',
+                           action='store',
+                           type=convert_to_complex,
                            dest='c')
 
     parser.add_argument('-z', '--zoom',
                         help='Degree by which the image is zoomed in. '
                              'The final zoom is affected by framerate and speed',
-                        type=float,
+                        action='store',
+                        type=convert_to_complex,
                         default=0)
 
     parser.add_argument('--framerate',
@@ -63,6 +70,11 @@ def parse():
                         ],
                         default='linear')
 
+    parser.add_argument('--cutoff',
+                        help='Number of iterations after which the colorization function repeats itself. '
+                             'Only used when the \'sin\' option is selected as the color function.',
+                        type=int)
+
     parser.add_argument('-r', '--resolution',
                         help='Resolution of the image to be generated.',
                         nargs=2,
@@ -80,32 +92,41 @@ def parse():
                         help='Return the required time to compute an image',
                         action='store_true')
 
-    return parser.parse_args()
+    args = sys.argv
+
+    for i, j in enumerate(args):
+        if len(j) < 2:
+            continue
+        if (j[1].isdigit() or j[1] == '.') and j[0] == '-':
+            args[i] = '@' + j[1:]
+
+    return parser.parse_args(args[1:])
 
 
 def main():
     args = parse()
-    focus = convert_to_complex(args.focus)
 
     start_time = time.time()
 
     if args.mandelbrot:
-        generator = MandelbrotGenerator(focus=focus,
+        generator = MandelbrotGenerator(focus=args.focus,
                                         zoom=args.zoom,
                                         resolution=args.resolution,
                                         framerate=args.framerate,
-                                        speed=args.speed)
+                                        speed=args.speed,
+                                        iterations=args.iterations)
 
     else:
-        generator = JuliaGenerator(focus=focus,
+        generator = JuliaGenerator(focus=args.focus,
                                    zoom=args.zoom,
                                    resolution=args.resolution,
                                    framerate=args.framerate,
                                    speed=args.speed,
-                                   c=convert_to_complex(args.c))
+                                   iterations=args.iterations,
+                                   c=args.c)
 
     imager = Imager(generator)
-    image = imager.generate_image(color_type=args.color)
+    image = imager.generate_image(color_type=args.color, cutoff=args.cutoff)
 
     if args.time:
         print('{} set generated in {} seconds'.format(
